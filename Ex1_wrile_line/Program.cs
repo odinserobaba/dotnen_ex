6169d9174c3a636112237bcaff911fd7f0e6110a
odinserobaba <odinserobaba@gmail.com>
2023-07-03 08:46:16 +0300

    Ex1_write_line text color

--- Ex1_wrile_line/Program.cs
+++ Ex1_wrile_line/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("This is a red text.");
+Console.ResetColor();
+
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.WriteLine("This text has a yellow background.");
+Console.ResetColor();
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+string name = "John";
+int age = 30;
+double height = 1.75;
+Console.WriteLine("Name: {0}, Age: {1}, Height: {2:F2}", name, age, height);
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("******************************");
+Console.WriteLine("   Welcome to My Console App   ");
+Console.WriteLine("******************************");
+Console.ResetColor();
